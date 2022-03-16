8ea47a9c815f4930c0e1f57cc2faab4505e4b242
Ian <chenghsi.yang@gmail.com>
2022-03-16 21:47:54 +0800

    adding a new project for development

---
+++ Development/translations/translation_en_US.ts
@@ -0,0 +1,47 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+    <context>
+        <name>Hello World/behavior.xar:/Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <location filename="../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../../Users/xcz/translations/Hello World/behavior.xar" line="0"/>
+            <source></source>
+            <comment>Text</comment>
+            <translation></translation>
+        </message>
+        <message>
+            <source>Hellow World</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Hellow World</translation>
+        </message>
+        <message>
+            <source>Hello World</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Hello World</translation>
+        </message>
+        <message>
+            <source>Hello World!</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Hello World!</translation>
+        </message>
+        <message>
+            <location filename="Hello World/behavior.xar" line="0"/>
+            <source>I am very difficult to work with!</source>
+            <comment>Text</comment>
+            <translation type="unfinished">I am very difficult to work with!</translation>
+        </message>
+    </context>
+    <context>
+        <name>Hello World/behavior.xar:/Say (1)</name>
+        <message>
+            <source>Hello World</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Hello World</translation>
+        </message>
+    </context>
+</TS>

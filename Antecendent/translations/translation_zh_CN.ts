c58b795c5369235cb2e5755b423814b730022228
Cheng-Hsi Yang <chenghsi.yang@gmail.com>
2022-06-14 22:42:40 +0800

    reconstructing animated speech

--- Antecendent/translations/translation_zh_CN.ts
+++ Antecendent/translations/translation_zh_CN.ts
@@ -2,6 +2,14 @@
 <!DOCTYPE TS>
 <TS version="2.1" language="zh_CN">
     <context>
+        <name>behavior_1/behavior.xar:/Animated Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">你好</translation>
+        </message>
+    </context>
+    <context>
         <name>behavior_1/behavior.xar:/C-Positive/2Second</name>
         <message>
             <location filename="behavior_1/behavior.xar" line="0"/>
--- Development/Development.pml
+++ Development/Development.pml
@@ -2,7 +2,7 @@
 <Package name="Development" format_version="4">
     <Manifest src="manifest.xml" />
     <BehaviorDescriptions>
-        <BehaviorDescription name="behavior" src="behavior_1" xar="behavior.xar" />
+        <BehaviorDescription name="behavior" src="main_mandarin" xar="behavior.xar" />
     </BehaviorDescriptions>
     <Dialogs />
     <Resources />
